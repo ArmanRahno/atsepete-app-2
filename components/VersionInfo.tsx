8d84f690011dbb57b040f78d759303f9d0ab332d
ArmanRahno <arman.rahno.matanagh@gmail.com>
2026-02-11 13:35:41 +0300

    Changed version number

--- components/VersionInfo.tsx
+++ components/VersionInfo.tsx
@@ -4,7 +4,7 @@ import { Text } from "react-native";
 const VersionInfo = () => {
 	return (
 		<Text className="mt-1 text-xs text-muted-foreground">
-			AtSepete @2025, v{Constants.expoConfig?.version} - 10.02.2026
+			AtSepete @2025, v{Constants.expoConfig?.version} - 11.02.2026
 		</Text>
 	);
 };
